c97a75c7f622f2aacf458f5d097c2daecc207360
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-03 14:20:32 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -37,10 +37,4 @@ def until_iterator(number_of_times)
  end
 end
 
-def for_iterator(number_of_times)
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  for number_of_times
-  puts phrase
-end
-end
 
