62cad9fac883e50ac9c406d50a1f6ce27d3e5e54
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-03 14:04:22 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 
-def times_iterator(number_of_times)
-  # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-end
+
 
 def while_iterator(number_of_times)
  phrase = "Welcome to Flatiron School's Web Development Course!"
