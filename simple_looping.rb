f85d96e77f27ad489709b95e30802497f3a4df64
vyskoutaris <vyskoutaris@hotmail.com>
2019-02-03 14:16:06 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -14,7 +14,8 @@ end
 
 def times_iterator(number_of_times)
    phrase = "Welcome to Flatiron School's Web Development Course!"
-   number_of_times.do 
+   number_of_times.times do 
+     puts phrase
  end
 
 def while_iterator(number_of_times)
